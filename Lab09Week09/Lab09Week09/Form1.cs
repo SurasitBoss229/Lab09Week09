a0ca6df2897ee218c97cf826e327d83ab5bf808b
Surasit <surasitboss229@gmail.com>
2017-10-22 11:43:05 +0700

    W09P22

--- Lab09Week09/Lab09Week09/Form1.cs
+++ Lab09Week09/Lab09Week09/Form1.cs
@@ -13,14 +13,26 @@ namespace Lab09Week09
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            Graphics g = e.Graphics;
+            g.Dispose();
+        }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            LinearGradientBrush pnlGdt = new LinearGradientBrush(panel1.ClientRectangle,
+                Color.Yellow, Color.Navy, 0f, true);
+            e.Graphics.FillRectangle(pnlGdt, panel1.ClientRectangle);
+            pnlGdt.Dispose();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            LinearGradientBrush pnlGdt = new LinearGradientBrush(panel2.ClientRectangle,
+                Color.Yellow, Color.Navy, 90f, true);
+            e.Graphics.FillRectangle(pnlGdt, panel2.ClientRectangle);
+            pnlGdt.Dispose();
         }
     }
 }
